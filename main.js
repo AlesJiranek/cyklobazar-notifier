886f37f5fcc2a8eaf9ea4cc14d9ec969b0edae44
Aleš Jiránek <a.jiranek@gmail.com>
2015-12-28 18:52:37 +0100

    Filter adds created/updated in last hour

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 import Promise from 'bluebird';
 import cheerio from 'cheerio';
 import fetch from 'isomorphic-fetch';
+import moment from 'moment';
+import querystring from 'querystring';
 
-fetch('http://www.cyklobazar.cz')
+const filters = {
+  type: 'sell',
+  'filter[list_wheel_diameter_id]' : 3,
+  priceMax: 25000
+};
+
+const addSelector = 'body > div.container > div.row > div.col-main > ul.media-list > li.media';
+
+fetch('http://www.cyklobazar.cz/horska-kola?' + querystring.stringify(filters))
   .then(response => response.text())
-  .then(html => cheerio.load(html));
+  .then(text => cheerio.load(text))
+  .then($ => $(addSelector))
+  .then(adds => adds.filter((i, add) => {
+    const dateTitle = cheerio(add).find('div.media-body > div.media-heading > small.pull-right').attr('title');
+    const dateString = dateTitle.split(' ').slice(1).join(' '); // Remove string at the beggining of title (Aktualizováno / Vytvořeno)
+    const addDate = moment(dateString, 'DD.MM.YYYY, HH:mm');
+    const dateToCompare = moment().subtract(1, 'hours');
+
+    return addDate.isAfter(dateToCompare);
+  }));
