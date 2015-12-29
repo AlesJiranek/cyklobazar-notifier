93c70b9e24ea8cfca8c2836ab1875ca842036b88
Aleš Jiránek <a.jiranek@gmail.com>
2015-12-29 01:11:26 +0100

    Don`t show .env missing file errors

--- src/email/index.js
+++ src/email/index.js
@@ -1,5 +1,5 @@
 require('babel-register');
-require('dotenv').load();
+require('dotenv').load({silent: true});
 
 if (!process.env.NOTIFICATION_EMAIL)
   throw new Error('Some of environment variables is not set. You have to set NOTIFICATION_EMAIL variable.');
--- src/push/index.js
+++ src/push/index.js
@@ -1,5 +1,5 @@
 require('babel-register');
-require('dotenv').load();
+require('dotenv').load({silent: true});
 
 if (!process.env.PUSHOVER_USER || !process.env.PUSHOVER_TOKEN)
   throw new Error('Some of environment variables is not set. You have to set both PUSHOVER_USER and PUSHOVER_TOKEN variables.');
